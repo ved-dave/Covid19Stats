216db4b4e4d5ddb1fcefada8b2484e878a08b9a3
Ved Dave <51809650+ved-dave@users.noreply.github.com>
2020-03-18 16:11:10 -0400

    Add files via upload

--- coronavirus.py
+++ coronavirus.py
@@ -1,7 +1,15 @@
 import numpy
 import scipy
 import csv
-from matplotlib.pyplot import*
+import matplotlib.pyplot as plt
+
+
+def graphingTest(myList):
+    x1 = list(range(0,len(myList)))
+    plt.plot(x1,myList)
+    plt.xlabel('Days')
+    plt.ylabel('Confirmed Victims')
+    plt.show()
 
 if __name__ == '__main__':
     with open('time_series_19-covid-Confirmed.csv') as csv_file: # load raw data from JHU
@@ -21,20 +29,12 @@ if __name__ == '__main__':
                     for elem in row:
                         if index > 3:
                             cases_list.append(elem) # add cases for each day to the list
-                            graphingTest(cases_list)
                             #print(cases_list, end='')
                         index += 1
                     print(cases_list)
+                    graphingTest(cases_list)
                     
 
-def graphingTest(myList)
-    for i in myList:
-        x1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-        y1 = mylist[i]
-        plt.plot(x1,y1,label = "line X")
-        plt.xlim(1,40)
-        plt.ylim(0,500)
-        plt.xlabel('Days')
-        plt.ylabel('Confirmed Victims')
-        plt.show()
+
+
 
