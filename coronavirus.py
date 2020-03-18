c9641a80325e82510b73559c72500e7ad13194c0
Abhijith Balijepalli <57305830+Rubber-Duck18@users.noreply.github.com>
2020-03-18 12:56:08 -0700

    Update coronavirus.py

--- coronavirus.py
+++ coronavirus.py
@@ -1,11 +1,7 @@
 import numpy
 import scipy
 import csv
-from matplotlib import *
-
-
-
-
+from matplotlib.pyplot import*
 
 if __name__ == '__main__':
     with open('time_series_19-covid-Confirmed.csv') as csv_file: # load raw data from JHU
@@ -25,10 +21,20 @@ if __name__ == '__main__':
                     for elem in row:
                         if index > 3:
                             cases_list.append(elem) # add cases for each day to the list
+                            graphingTest(cases_list)
                             #print(cases_list, end='')
                         index += 1
                     print(cases_list)
                     
 
-
+def graphingTest(myList)
+    for i in myList:
+        x1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        y1 = mylist[i]
+        plt.plot(x1,y1,label = "line X")
+        plt.xlim(1,40)
+        plt.ylim(0,500)
+        plt.xlabel('Days')
+        plt.ylabel('Confirmed Victims')
+        plt.show()
 
