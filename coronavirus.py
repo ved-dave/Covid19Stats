ceddac16c9cab66ddcdd8cb418f9904b2815eeb5
Ved Dave <51809650+ved-dave@users.noreply.github.com>
2020-03-18 17:05:37 -0400

    Add files via upload

--- coronavirus.py
+++ coronavirus.py
@@ -1,15 +1,18 @@
 import numpy
+import math
 import scipy
 import csv
 import matplotlib.pyplot as plt
 
 
 def graphingTest(myList):
-    x1 = list(range(0,len(myList)))
-    plt.plot(x1,myList, 'ro')
+    #x1 = list(range(0,len(myList)))
+    plt.figure()
+    x = list(range(0, len(myList)))
+    print(max(myList))
+    plt.scatter(x, myList)
     plt.xlabel('Days since 1/22/20')
     plt.ylabel('Confirmed Cases')
-    #plt.yscale('linear')
     plt.title("[country name]")
     #plt.autoscale(enable=True, axis='y', tight=true)
     plt.show()
@@ -31,7 +34,7 @@ if __name__ == '__main__':
                     cases_list = [] # create a list for location
                     for elem in row:
                         if index > 3:
-                            cases_list.append(elem) # add cases for each day to the list
+                            cases_list.append(int(elem)) # add cases for each day to the list
                             #print(cases_list, end='')
                         index += 1
                     print(cases_list)
