8d04a86be7ef7e58f0a8dd867a8099042ab5224a
Ved Dave <51809650+ved-dave@users.noreply.github.com>
2020-03-18 15:40:48 -0400

    Add files via upload
    documentation

--- coronavirus.py
+++ coronavirus.py
@@ -1,33 +1,34 @@
 import numpy
 import scipy
 import csv
-from matplotlib.pyplot import*
+from matplotlib import *
 
 
 
 
 
 if __name__ == '__main__':
-    with open('time_series_19-covid-Confirmed.csv') as csv_file:
+    with open('time_series_19-covid-Confirmed.csv') as csv_file: # load raw data from JHU
             csv_reader = csv.reader(csv_file)
             line_count = 0
-            for row in csv_reader:
-                if line_count == 0:
-                    print('The countries/regions are')
+            for row in csv_reader: # iterate through each location
+                if line_count == 0: # initialize first row of dates
+                    print('The countries/regions are:')
                     line_count += 1
                 else:
-                    if not row[0]:
-                        print('\tLocation: {}. '.format(row[1]), end = '')
+                    if not row[0]: # check if location has a subregion
+                        print('\tLocation: {}. '.format(row[1]), end= '')
                     else:
-                        print('\tLocation: {}, {}'.format(row[0], row[1]), end = '')
+                        print('\tLocation: {}, {}'.format(row[0], row[1]), end= '')
                     index = 0
-                    cases_list = []
+                    cases_list = [] # create a list for location
                     for elem in row:
                         if index > 3:
-                            cases_list.append(elem)
-                            #print(cases_list)
+                            cases_list.append(elem) # add cases for each day to the list
+                            #print(cases_list, end='')
                         index += 1
                     print(cases_list)
+                    
 
 
-        
+
